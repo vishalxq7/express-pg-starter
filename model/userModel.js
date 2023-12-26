559f4e4302599987e09dcc62bb7acb1c10d04fcd
Vishal Gaikwad <vishalofl@gmail.com>
2023-12-26 10:54:39 +0530

    nodemon added

--- model/userModel.js
+++ model/userModel.js
@@ -1,15 +1,13 @@
-const db = require('../config/dbSetup-pool');
+const db = require("../config/dbSetup-pool");
 // const db = require('../config/dbSetup-client');
 
 const getUserData = async () => {
+  const query1 = `select * from users`;
+  const queryIns = `insert into users ("username","firstName","lastName") values($1,$2,$3) returning id`;
 
-    const query1 = `select * from users where username = 'vishalofl567'`;
-    const queryIns = `insert into users ("username","firstName","lastName") values($1,$2,$3) returning id`;
+  let resp = await db.query(query1, []);
 
-    let resp = await db.query(query1,[]);
+  return resp.rows;
+};
 
-    return resp.rows;
-
-}
-
-module.exports = { getUserData }
+module.exports = { getUserData };
